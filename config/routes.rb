df104441a5030c6f4682ef0bd4d491d318e18344
Artur Woźniak <artur.wozniak69@gmail.com>
2024-01-06 13:16:59 +0100

    style line code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :carts
-  root 'store#index', as: 'store_index'
+  root "store#index", as: "store_index"
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -17,7 +17,7 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create cart" do
     assert_difference("Cart.count") do
-      post carts_url, params: { cart: {  } }
+      post carts_url, params: { cart: {} }
     end
 
     assert_redirected_to cart_url(Cart.last)
@@ -34,7 +34,7 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update cart" do
-    patch cart_url(@cart), params: { cart: {  } }
+    patch cart_url(@cart), params: { cart: {} }
     assert_redirected_to cart_url(@cart)
   end
 
